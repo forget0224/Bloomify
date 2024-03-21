bd1534d1a5d163ba579d805697dd340914320423
lulu <forget0224@gmail.com>
2024-03-21 22:35:36 +0800

    add/component-btn

--- pages/template.js
+++ pages/template.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import DefaultLayout from '@/components/layout/default-layout'
 import bannerFlower from '@/assets/banner-flower.jpg'
+import { MyButton } from '@/components/btn/mybutton'
 import { Card, CardHeader, CardBody, Image as ImageUI } from '@nextui-org/react'
 export default function Custom() {
   const [activePage, setActivePage] = useState('custom')
@@ -10,8 +11,38 @@ export default function Custom() {
       {
         <>
           {/* main的東西 */}
-          <div className="w-screen h-screen bg-orange-100 text-2xl text-black">
-            代客送花
+          {/* text-XXX 文字顏色 */}
+          <div className="w-screen h-screen bg-orange-100  text-black">
+            <p className="text-2xl text-danger">代客送花</p>
+            <p className="text-2xl text-tertiary-gray-200">代客送花</p>
+            <p className="text-2xl text-tertiary-blue">代客送花</p>
+            <p className="text-2xl text-primary-100">代客送花</p>
+            <p className="text-2xl text-primary-200">代客送花</p>
+            <p className="text-2xl text-primary-300">代客送花</p>
+            <MyButton color="secondary" size="md">
+              加入我們ddd
+            </MyButton>
+            <MyButton color="primary" size="xl">
+              加入我們xl
+            </MyButton>
+            <MyButton color="secondary" size="xs">
+              加入我們xs
+            </MyButton>
+            <MyButton color="secondary" size="md">
+              加入我們md
+            </MyButton>
+            <MyButton color="primary" size="xl" isDisabled>
+              加入我們xl
+            </MyButton>
+            <MyButton color="primary" size="xs" isOutline>
+              加入我們xs
+            </MyButton>
+            <MyButton color="secondary" size="md" isOutline>
+              加入我們md
+            </MyButton>
+            <MyButton color="primary" size="xl" isOutline>
+              加入我們xl
+            </MyButton>
           </div>
 
           <div className="w-screen h-screen bg-blue-100  text-black flex flex-col  justify-center items-center">
