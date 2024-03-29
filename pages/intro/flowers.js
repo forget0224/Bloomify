b70590d75d716a6aeea8e3a6879f813f542ea5c7
will <willchang.mkt@gmail.com>
2024-03-29 18:59:42 +0800

    花圖鑑初步架構

--- pages/intro/flowers.js
+++ pages/intro/flowers.js
@@ -1,117 +1,70 @@
 import { useState } from 'react'
 import DefaultLayout from '@/components/layout/default-layout'
-import styles from '../custom/custom.module.css'
-import Image from 'next/image'
-import bannerFlower from '@/assets/banner-flower.jpg'
-import { Card, CardBody, CardFooter } from '@nextui-org/react'
-//import {Image} from "@nextui-org/react";
-export default function Intro() {
-  const [activePage, setActivePage] = useState('intro')
-  const list = [
-    {
-      title: 'Orange',
-      img: '/images/fruit-1.jpeg',
-      price: '$5.50',
-    },
-    {
-      title: 'Tangerine',
-      img: '/images/fruit-2.jpeg',
-      price: '$3.00',
-    },
-    {
-      title: 'Raspberry',
-      img: '/images/fruit-3.jpeg',
-      price: '$10.00',
-    },
-    {
-      title: 'Lemon',
-      img: '/fruit-4.jpeg',
-      price: '$5.30',
-    },
-    {
-      title: 'Avocado',
-      img: '/images/fruit-5.jpeg',
-      price: '$15.70',
-    },
-    {
-      title: 'Lemon 2',
-      img: '/images/fruit-6.jpeg',
-      price: '$8.00',
-    },
-    {
-      title: 'Banana',
-      img: '/images/fruit-7.jpeg',
-      price: '$7.50',
-    },
-    {
-      title: 'Watermelon',
-      img: '/images/fruit-7.jpeg',
-      price: '$12.20',
-    },
-  ]
+import { Image } from '@nextui-org/react'
+import { MyButton } from '@/components/btn/mybutton'
+import CardGroup from '@/components/intro/card-group'
+
+import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react'
+import Subtitle from '@/components/intro/subtitle'
+import ButtonGL from '@/components/intro/buttonGL'
+
+export default function CourseIndex() {
+  const [activePage, setActivePage] = useState('course')
+  const underlines = ['none']
   return (
-    <DefaultLayout activePage={activePage}>
-      {
-        <>
-          <div className="w-full">
-            <Image
-              src={'/intro_flowers_banner.png'} // 使用 require 導入圖片
-              alt="花束圖片"
-              width={1520}
-              height={730}
-            />
-          </div>
-          {/* main的東西 */}
-          {/* <div className="w-screen h-screen bg-orange-100 text-2xl text-black">
-            霹靂卡霹靂拉拉波波力那貝貝魯多
-          </div> */}
-          <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-            {list.map((item, index) => (
-              <Card
-                shadow="sm"
-                key={index}
-                isPressable
-                onPress={() => console.log('item pressed')}
-              >
-                <CardBody className="overflow-visible p-0">
-                  <Image
-                    shadow="sm"
-                    radius="lg"
-                    width={400}
-                    height={400}
-                    alt={item.title}
-                    className="w-full object-cover h-[140px]"
-                    src={item.img}
-                  />
-                </CardBody>
-                <CardFooter className="text-small justify-between">
-                  <b>{item.title}</b>
-                  <p className="text-default-500">{item.price}</p>
-                </CardFooter>
-              </Card>
+    <DefaultLayout
+      activePage={activePage}
+      className="justify-center flex-col items-center"
+    >
+      <main className="bg-white flex justify-center items-center">
+        <div className="container justify-center flex-col items-center">
+          {/* 麵包屑 */}
+          <div className="bg-white flex flex-col flex-wrap gap-4 py-6">
+            {underlines.map((u) => (
+              <div key={u}>
+                <Breadcrumbs underline={u}>
+                  <BreadcrumbItem className="font-serif">首頁</BreadcrumbItem>
+                  <BreadcrumbItem className="font-serif">
+                    花與遊戲
+                  </BreadcrumbItem>
+                  <BreadcrumbItem color="primary">花圖鑑</BreadcrumbItem>
+                </Breadcrumbs>
+              </div>
             ))}
           </div>
-          <div className={styles['foo']}>代客送花 建立custom branch</div>
-          <div className="w-screen h-screen bg-blue-100  text-black flex flex-col  justify-center items-center">
-            <div className="border-1 border-pink w-[1000px] h-[400px] flex flex-row">
-              <div className="w-full flex flex-col bg-white justify-center items-center text-center">
-                <h1 className="text-2xl my-3">情人節活動</h1>
-                <p className="my-3 px-4">
-                  無論風雨，無論時節，花店都在為您提供最溫馨的服務。無論風雨，無論時節，花店都在為您提供最溫馨的服務。無論風雨，無論時節，花店都在為您提供最溫馨的服務。
-                </p>
-                <div className="w-full text-right px-4 my-3">
-                  <a href="#" className="text-black ">
-                    More
-                  </a>
-                </div>
-              </div>
-              <div className="w-full">
-                <Image src={bannerFlower} alt="" className="w-[500px] h-full" />
-              </div>
+
+          {/* banner圖 */}
+          <Image
+            width={1520}
+            alt="花圖鑑首頁banner圖"
+            src="/assets/intro/intro_flower_banner.png"
+            className="rounded-2xl"
+          />
+          <div className="m-8">
+            <Subtitle text="花圖鑑" />
+          </div>
+
+          {/* 卡片群組 */}
+          <div className="grid gap-y-4 my-14 ">
+            <CardGroup />
+          </div>
+          <div className="grid gap-y-4 my-14 ">
+            <CardGroup />
+          </div>
+          <div className="grid gap-y-4 my-14 ">
+            <CardGroup />
+          </div>
+          <div className="grid gap-y-4 my-14 ">
+            <CardGroup />
+          </div>
+          <div className="grid gap-y-4 my-14 ">
+            <CardGroup />
+            <div class="flex justify-center">
+              <ButtonGL text="查看更多"></ButtonGL>
             </div>
           </div>
-        </>
-      }
+        </div>
+      </main>
     </DefaultLayout>
   )
 }
