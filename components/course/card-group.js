2365520afebbd83b821dcabd1ebbd308e41f7b16
simpleme37 <simpleme307@gmail.com>
2024-03-31 16:54:43 +0800

    update

--- components/course/card-group.js
+++ components/course/card-group.js
@@ -6,76 +6,81 @@ import {
   CardFooter,
   Image,
 } from '@nextui-org/react'
+import Link from 'next/link'
 
 import { BsFillStarFill } from 'react-icons/bs'
-import { BsHeart } from 'react-icons/bs'
+import { BsFillHeartFill } from 'react-icons/bs'
 
 export default function CardGroup() {
   const list = [
     {
       title: '課程一',
-      content: '課程內容課程內容課程內容課程內容',
+      content:
+        '課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容',
       img: '/assets/course/img_course_card_01.png',
-      star: '5',
+      star: '5.0',
       price: 'NT$1200',
     },
     {
       title: '課程二',
-      content: '課程內容課程內容課程內容課程內容',
+      content:
+        '課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容',
       img: '/assets/course/img_course_card_02.png',
-      star: '5',
+      star: '5.0',
       price: 'NT$1000',
     },
     {
       title: '課程三',
-      content: '課程內容課程內容課程內容課程內容',
+      content:
+        '課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容',
       img: '/assets/course/img_course_card_03.png',
-      star: '5',
+      star: '5.0',
       price: 'NT$800',
     },
     {
       title: '課程四',
-      content: '課程內容課程內容課程內容課程內容',
+      content:
+        '課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容課程內容',
       img: '/assets/course/img_course_card_04.png',
-      star: '5',
+      star: '5.0',
       price: 'NT$600',
     },
   ]
 
   return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+    <div className="gap- grid grid-cols-2 sm:grid-cols-4 gap-6">
       {list.map((item, index) => (
         <Card
           shadow="sm"
           key={index}
           isPressable
           onPress={() => console.log('item pressed')}
+          className="relative"
         >
-          <CardBody className="static overflow-visible p-0">
-            <Image
-              shadow="none"
-              radius="none"
-              width="100%"
-              alt={item.title}
-              className="w-full object-cover h-[140px]"
-              src={item.img}
-            />
-          </CardBody>
-          <CardHeader className="block text-left">
-            <div>
-              <p class="text-xl truncate ...">{item.title}</p>
-            </div>
-            <div>
-              <p class="text-base truncate ...">{item.content}</p>
-            </div>
-          </CardHeader>
-          <CardFooter className="text-small justify-between">
-            <p className="text-base flex">
-              <BsFillStarFill className="text-secondary-100" />
-              {item.star}
-            </p>
-            <p className="text-base">{item.price}</p>
-          </CardFooter>
+          <Link href="/course/details" key={index} className="block relative">
+            <CardBody className="static overflow-visible p-0">
+              <Image
+                shadow="none"
+                radius="none"
+                width="100%"
+                alt={item.title}
+                className="w-full object-cover h-[140px]"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="block text-left">
+              <p class="text-xl truncate mb-2">{item.title}</p>
+              <p class="line-clamp-2 mb-2">{item.content}</p>
+              <div className="text-small flex justify-between pt-0">
+                <p className="text-lg">{item.price}</p>
+                <p className="text-base flex items-center">
+                  <BsFillStarFill className="text-secondary-100 mr-1" />
+                  {item.star}
+                </p>
+              </div>
+            </CardFooter>
+            <BsFillHeartFill className="text-secondary-100 absolute top-4 right-4 z-40 w-6 h-6" />
+          </Link>
         </Card>
       ))}
     </div>
--- pages/course/index.js
+++ pages/course/index.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import DefaultLayout from '@/components/layout/default-layout'
 import { Image } from '@nextui-org/react'
-import { MyButton } from '@/components/btn/mybutton'
 import CardGroup from '@/components/course/card-group'
+import { Card, CardHeader, CardBody, CardFooter, Link } from '@nextui-org/react'
 
 import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react'
 import Subtitle from '@/components/course/subtitle'
@@ -13,48 +13,66 @@ export default function CourseIndex() {
   return (
     <DefaultLayout
       activePage={activePage}
-      className="justify-center flex-col items-center"
+      className="flex flex-col justify-center items-center"
     >
-      <main className="bg-white flex justify-center items-center">
-        <div className="container justify-center flex-col items-center">
+      {/* 置中 & 背景色 */}
+      <main className="flex flex-col justify-center items-center bg-white">
+        {/* 主要容器 */}
+        <div className="bg-white container justify-center flex flex-col items-start columns-12 static">
           {/* 麵包屑 */}
-          <div className="bg-white flex flex-col flex-wrap gap-4 py-6">
+          <div className="bg-white flex flex-col flex-wrap gap-4 py-6 w-full">
             {underlines.map((u) => (
               <div key={u}>
                 <Breadcrumbs underline={u}>
-                  <BreadcrumbItem className="font-serif">首頁</BreadcrumbItem>
+                  <BreadcrumbItem>首頁</BreadcrumbItem>
                   <BreadcrumbItem color="primary">合作課程</BreadcrumbItem>
                 </Breadcrumbs>
               </div>
             ))}
           </div>
 
-          {/* banner圖 */}
-          <Image
-            width={1520}
-            alt="課程首頁banner圖"
-            src="/assets/course/img_course_banner.png"
-            className="rounded-2xl"
-          />
+          {/* banner圖 & 加入我們 */}
+          <div className="relative w-full">
+            {/* banner圖 */}
+            <Image
+              alt="課程首頁banner圖"
+              src="/assets/course/img_course_banner.png"
+              className="rounded-2xl"
+            />
+            {/* 加入我們 */}
+            <Card className="flex flex-row p-6 columns-2 m-4 md:m-10 absolute top-0 left-0 z-40 rounded-lg">
+              <CardBody className="p-0">
+                <div className="flex flex-col gap-2 items-start justify-between ">
+                  <p>與我們一起提供優質的線下課程，讓更多人愛上花藝</p>
+                  <Link
+                    href="/pages/join"
+                    className="text-primary-100 mb-1 border-b-1 border-primary-100"
+                  >
+                    加入我們
+                  </Link>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
 
           {/* 卡片群組 */}
-          <div className="grid gap-y-4 my-14 ">
+          <div className="grid gap-y-4 my-14 w-full">
             <Subtitle text="最新課程" />
             <CardGroup />
           </div>
-          <div className="grid gap-y-4 my-14 ">
+          <div className="grid gap-y-4 my-14 w-full">
             <Subtitle text="熱銷課程" />
             <CardGroup />
           </div>
-          <div className="grid gap-y-4 my-14 ">
+          <div className="grid gap-y-4 my-14 w-full">
             <Subtitle text="精選商家" />
             <CardGroup />
           </div>
-          <div className="grid gap-y-4 my-14 ">
+          <div className="grid gap-y-4 my-14 w-full">
             <Subtitle text="四大分類" />
             <CardGroup />
           </div>
-          <div className="grid gap-y-4 my-14 ">
+          <div className="grid gap-y-4 my-14 w-full">
             <Subtitle text="為您推薦" />
             <CardGroup />
           </div>
