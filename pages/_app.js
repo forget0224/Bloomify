10583bb14aab5242a0ca51233b857f6a2de4cf22
lulu <forget0224@gmail.com>
2024-04-17 01:41:52 +0800

    add loading-animate

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,16 @@ import * as React from 'react'
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react'
-
+import { LoaderProvider } from '@/hooks/use-loader'
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
   return getLayout(
     <NextUIProvider>
-      <Component {...pageProps} />
+      <LoaderProvider>
+        <Component {...pageProps} />
+      </LoaderProvider>
     </NextUIProvider>
   )
 }
