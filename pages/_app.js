06d6c737c5581b9f9e46ffa1ee72451ad60c4448
cheni0915 <easy09150915@gmail.com>
2024-04-18 00:28:39 +0800

    use-auth hook

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,18 @@ import { AuthProvider } from '@/hooks/use-auth'
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react'
-
+import { LoaderProvider } from '@/hooks/use-loader'
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
   return getLayout(
-    <AuthProvider>
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </AuthProvider>
+    <NextUIProvider>
+      <AuthProvider>
+        <LoaderProvider>
+          <Component {...pageProps} />
+        </LoaderProvider>
+      </AuthProvider>
+    </NextUIProvider>
   )
 }
