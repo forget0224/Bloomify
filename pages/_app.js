53785f92eaf1ef5f94df62c78f29f44662115372
lulu <forget0224@gmail.com>
2024-04-19 04:44:50 +0800

    add/role&occ&color provider

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import * as React from 'react'
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react'
 import { LoaderProvider } from '@/hooks/use-loader'
+import { RoleProvider } from '@/hooks/use-role'
+import { ColorProvider } from '@/hooks/use-color'
+import { OccProvider } from '@/hooks/use-occ'
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
@@ -11,7 +14,13 @@ export default function MyApp({ Component, pageProps }) {
   return getLayout(
     <NextUIProvider>
       <LoaderProvider>
-        <Component {...pageProps} />
+        <ColorProvider>
+          <OccProvider>
+            <RoleProvider>
+              <Component {...pageProps} />
+            </RoleProvider>
+          </OccProvider>
+        </ColorProvider>
       </LoaderProvider>
     </NextUIProvider>
   )
