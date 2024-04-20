0c47dcda2bf47d09c665a3baa26a39e0d7a4c66e
lulu <forget0224@gmail.com>
2024-04-21 02:32:08 +0800

    layer connect canvas ok

--- components/custom/custom/LayerContent.js
+++ components/custom/custom/LayerContent.js
@@ -1,5 +1,126 @@
+// import React from 'react'
+
+// import {
+//   CiLock,
+//   CiUnlock,
+//   CiTrash,
+//   CiLineHeight,
+//   CiSquarePlus,
+// } from 'react-icons/ci'
+
+// export default function LayerContent() {
+//   return (
+//     <div className="text-tertiary-black w-full h-full flex flex-col justify-center items-center">
+//       <div className="text-tertiary-gray-100 w-60 text-center py-4">
+//         <p className="text-xs">ssdlkfskldhfsldkfhs</p>
+//       </div>
+//       <div className="h-auto w-full">
+//         <div className="px-3 py-2">
+//           {/* 整組list */}
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiLock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//           <div className="flex flex-row items-center justify-around border-b-1 py-2">
+//             <div className="text-2xl flex flex-row gap-2">
+//               <CiLineHeight />
+//               <div className="h-6 w-6 bg-primary"></div>
+//             </div>
+//             <div>picture</div>
+//             <div className="flex flex-row gap-3 text-2xl">
+//               <CiUnlock />
+//               <CiSquarePlus />
+//               <CiTrash />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 import React from 'react'
 
+import { useFlower } from '@/hooks/use-flower'
 import {
   CiLock,
   CiUnlock,
@@ -7,113 +128,41 @@ import {
   CiLineHeight,
   CiSquarePlus,
 } from 'react-icons/ci'
+import Image from 'next/image'
 
 export default function LayerContent() {
+  const { imagesInfo } = useFlower()
+  console.log(imagesInfo)
   return (
     <div className="text-tertiary-black w-full h-full flex flex-col justify-center items-center">
       <div className="text-tertiary-gray-100 w-60 text-center py-4">
-        <p className="text-xs">ssdlkfskldhfsldkfhs</p>
+        <p className="text-xs">Manage your flower layers below:</p>
       </div>
       <div className="h-auto w-full">
-        <div className="px-3 py-2">
-          {/* 整組list */}
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
+        <div className="px-3 py-2"></div>
+        {imagesInfo ? (
+          imagesInfo.map((img, index) => (
+            <div
+              key={index}
+              className="flex flex-row items-center justify-around border-b-1 py-2"
+            >
+              <div className="text-2xl flex flex-row gap-2">
+                <CiLineHeight />
+                <div className="h-6 w-6 ">
+                  <Image src={img.url} alt="Loading" height={24} width={24} />
+                </div>
+              </div>
+              <div>{img.name}</div>
+              <div className="flex flex-row gap-3 text-2xl">
+                <CiLock />
+                <CiSquarePlus />
+                <CiTrash />
+              </div>
             </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiLock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-around border-b-1 py-2">
-            <div className="text-2xl flex flex-row gap-2">
-              <CiLineHeight />
-              <div className="h-6 w-6 bg-primary"></div>
-            </div>
-            <div>picture</div>
-            <div className="flex flex-row gap-3 text-2xl">
-              <CiUnlock />
-              <CiSquarePlus />
-              <CiTrash />
-            </div>
-          </div>
-        </div>
+          ))
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   )
