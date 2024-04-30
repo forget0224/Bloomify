d6bdfb8d5d1b8a5a767c3decfc445982976ca92a
simpleme37 <simpleme307@gmail.com>
2024-04-30 19:29:03 +0800

    fix share-modal

--- components/layout/center-layout.js
+++ components/layout/center-layout.js
@@ -4,7 +4,7 @@ export default function CenterLayout({ children, activePage }) {
       {/* 置中 & 背景色 */}
       <main className="flex flex-col justify-center items-center bg-white">
         {/* 主要容器 */}
-        <div className="bg-white container justify-center flex flex-col items-center columns-12 px-5 sm:mt-8 md:mt-0 sm:mb-10 md:px-0 my-4">
+        <div className="bg-white container justify-center flex flex-col items-center columns-12 px-5 sm:mt-8 md:mt-0 sm:mb-10 md:mb-20 md:px-0 my-4">
           {children}
         </div>
       </main>
