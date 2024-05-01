2bb672596cfc1dac09105afe2505d5f933e093c1
lulu <forget0224@gmail.com>
2024-05-01 17:24:51 +0800

    fixed/loading svg/img

--- components/common/loader.js
+++ components/common/loader.js
@@ -1,12 +1,10 @@
-import { useEffect } from 'react'
-import loading from '@/assets/loading-css.svg'
-import Image from 'next/image'
+import Loading from '@/assets/loading-css.svg'
 export default function Loader() {
   // return <Loading />
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center bg-white">
-        <Image src={loading} alt="Loading" height={300} />
+        <Loading height={300} />
       </div>
     </>
   )
